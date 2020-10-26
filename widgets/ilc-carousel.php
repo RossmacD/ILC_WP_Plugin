6fe39d583143c1c5cb8e68920fa332fd1846f4b6
RMD@ILC <rossmacdonald3411@gmail.com>
2020-10-26 11:47:02 +0000

    Bookmark js now gives feedback when added "

--- widgets/ilc-carousel.php
+++ widgets/ilc-carousel.php
@@ -121,6 +121,11 @@ class ILC_Carousel extends Widget_Base {
 			]
         );
 
+        
+        // $query = new \WP_Query(array(
+        //     'post_type' => 'recipe',
+        // ));
+
         $this->add_control(
 			'selected_posts',
 			[
@@ -204,14 +209,15 @@ class ILC_Carousel extends Widget_Base {
                         while ( $query->have_posts() ) { 
                             $query->the_post();
 			                $skill_level = get_the_terms(get_the_id(), 'skill_level');
-                            // $pro = mb_strimwidth(the_excerpt(), 0, 10, "...");
                         ?>
                         <section class="item elementor-section elementor-section-boxed splide__slide" style=<?php echo '"background-image: url(\''. wp_get_attachment_image_src( get_post_thumbnail_id(get_the_id()), 'small' )[0] .'\');"'?>>
                             <div class="elementor-container cardPos">
                             <h1 style="position:absolute;top:10%;right:25px;" onclick=<?php echo '"bookmark(\''. get_permalink() .'\','. get_the_id().')"'?>>
-                                <svg width="39" height="42" viewBox="0 0 39 42" class="bookmarkRecipe" fill="none" xmlns="http://www.w3.org/2000/svg">
+                            <!-- <a id="bookmarkme" href="#" rel="sidebar" title="bookmark this page"> -->
+                                <svg id=<?php echo '"bkmk-'. get_the_id().'"' ?> width="39" height="42" viewBox="0 0 39 42" class="bookmarkRecipe" fill="none" xmlns="http://www.w3.org/2000/svg">
                                     <path d="M3.76901 2.51267H35.231C35.9249 2.51267 36.4873 3.07515 36.4873 3.76901V38.0852C36.4873 39.1956 35.1523 39.7601 34.3557 38.9864L23.8766 28.8085C21.4391 26.441 17.5609 26.4411 15.1234 28.8085L4.64432 38.9864C3.84774 39.7601 2.51267 39.1957 2.51267 38.0852V3.76901C2.51267 3.07515 3.07515 2.51267 3.76901 2.51267Z" stroke="white" stroke-width="5.02534"/>
                                 </svg>
+                                <!--  </a> -->
                             </h1>
 
                             <div class="ilc-catLabel">salads</div>
@@ -220,7 +226,7 @@ class ILC_Carousel extends Widget_Base {
                                     <div class="ilc-cardtext">
                                         <h3><?php echo get_the_title() ?></h3>
                                         <p><?php echo implode('. ', array_slice(explode('.', get_the_excerpt()), 0, 2)) . '.'?></p>
-                                    </div>
+					                </div>
                                     <div class="ilc-cardinfo">
                                         <p><svg width="25" height="21" viewBox="0 0 25 21" fill="none" xmlns="http://www.w3.org/2000/svg">
                                             <path d="M19.3063 20.6643H5.91404C5.69791 20.6643 5.5281 20.4945 5.5281 20.2784V14.6745C2.99631 14.4815 0.997131 12.3666 0.997131 9.78846C0.997131 7.07914 3.197 4.87927 5.90632 4.87927C6.52383 4.87927 7.14134 4.99505 7.72025 5.22662C7.91322 2.70255 10.0282 0.703369 12.6063 0.703369C15.1844 0.703369 17.3071 2.70255 17.5 5.23434C18.079 5.00277 18.6887 4.88699 19.314 4.88699C22.0233 4.88699 24.2232 7.08686 24.2232 9.79618C24.2232 12.3743 22.224 14.497 19.6922 14.6899V20.2861C19.6922 20.4945 19.5147 20.6643 19.3063 20.6643ZM6.29998 19.8924H18.9203V14.304C18.9203 14.0879 19.0901 13.918 19.3063 13.918C21.591 13.918 23.4513 12.0655 23.4513 9.78074C23.4513 7.49596 21.5988 5.64344 19.314 5.64344C18.6116 5.64344 17.9169 5.82097 17.3071 6.16832C17.1836 6.23779 17.0369 6.23007 16.9134 6.1606C16.7976 6.08341 16.7282 5.95219 16.7282 5.81325L16.7359 5.72834C16.7359 5.68975 16.7436 5.64344 16.7436 5.60484C16.7436 3.32778 14.8911 1.47525 12.6063 1.47525C10.3215 1.47525 8.4767 3.32778 8.4767 5.61256C8.4767 5.65887 8.4767 5.69747 8.48442 5.73606C8.48442 5.73606 8.49214 5.81325 8.49214 5.82097C8.49986 5.95991 8.43039 6.09113 8.30689 6.16832C8.1911 6.24551 8.03673 6.24551 7.91322 6.17604C7.30343 5.83641 6.60874 5.65115 5.90632 5.65115C3.62154 5.65115 1.76902 7.5114 1.76902 9.78846C1.76902 12.0655 3.62154 13.9258 5.90632 13.9258C6.12245 13.9258 6.29998 14.0956 6.29998 14.3117V19.8924Z" fill="#E30062"/>
@@ -405,20 +411,17 @@ class ILC_Carousel extends Widget_Base {
         </style>
 
         <script>
-        const bookmark=(link,id)=>{
-            fetch(link+'?wpfpaction=add&postid='+id+'&ajax=1',{
-                headers : { 
-                    'Content-Type': 'application/json',
-                    'Accept': 'application/json'
-                }
-
-                })
+        const bookmark=(link,id,action="add")=>{
+            fetch(link+'?wpfpaction='+action+'&postid='+id+'&ajax=1',)
                 .then(response => {
-                    if(response.status===200)return response.json()
-                    console.log(response)
-                    throw new Error('Ah shit')
+                    if(response.status===200){
+                        document.querySelector('#bkmk-'+id).classList.add('active')
+                        return
+                    }
+                    // console.log(response)
+                    // You must be logged in
+                    throw new Error('You must be logged in')
                     })
-                .then(data => console.log(data))
                 .catch(err=>console.error(err));
         }
 
@@ -448,6 +451,7 @@ class ILC_Carousel extends Widget_Base {
 	protected function _content_template() {
 		?>
 		<div class="title">
+            <p>Refresh to see updated version</p>
 			{{{ settings.title }}}
 		</div>
 		<?php
